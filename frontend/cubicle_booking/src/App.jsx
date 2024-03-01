669e4aed4813af45b6d75bd503b016463d62ddcf
shaky008 <sshakya.uutsav@gmail.com>
2024-03-01 03:39:38 -0800

    added HomePage

--- frontend/cubicle_booking/src/App.jsx
+++ frontend/cubicle_booking/src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import { Button } from "@nextui-org/react";
+import HomePage from "./pages/HomePage";
+
 function App() {
   return (
     <>
-      <div className="flex gap-4 items-center">
-        <Button size="sm">Small</Button>
-        <Button size="md">Medium</Button>
-        <Button size="lg">Large</Button>
-      </div>
+      <HomePage />
     </>
   );
 }
